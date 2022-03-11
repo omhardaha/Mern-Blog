6e242b28ec9e20a6479ad16228b5b0a65b895c1e
omhardaha <omprakashhardaha369@gmail.com>
2022-03-11 19:29:43 +0530

    updates

--- omblog/src/App.js
+++ omblog/src/App.js
@@ -1,20 +1,30 @@
 import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+
 import Topbar from "./topbar/Topbar";
 import Home from "./pages/Home/Home";
 import Single from "./Single/Single";
 import Write from "./Write/Write";
 import Setting from "./Setting/Setting";
 import Login from "./Login/Login";
+import Register from "./Register/Register";
 function App() {
 	return (
-		<div className="App">
+		<>
 			<Topbar />
+			{/* <Route exact path="/" element={<Home />} />
+			<Route exact path="Login" element={<Login />} /> */}
 			{/* <Home /> */}
 			{/* <Single /> */}
-            {/* <Write/> */}
-            {/* <Setting/>  */}
-            <Login/>
-		</div>
+			{/* <Write/> */}
+			{/* <Setting/>  */}
+			{/* <Login/> */}
+			<Routes>
+				{/* <Route path="/" element={<Home />} /> */}
+				<Route path="/login" element={Login} />
+			</Routes>
+			<Register />
+		</>
 	);
 }
 
--- omblog/src/Register/Register.jsx
+++ omblog/src/Register/Register.jsx
@@ -1,17 +1,19 @@
-import "./Login.css"
+import "./Register.css"
 
 export default function Login() {
     return (
         <div className="login">
-            <span className="loginTitle">Login</span>
+            <span className="loginTitle">Register</span>
             <form action="" className="loginForm">
+                <label htmlFor=""></label>
+                <input type="text" placeholder="Enter You UserName" />
                 <label htmlFor=""></label>
                 <input type="text" placeholder="Enter You Email" />
                 <label htmlFor=""></label>
                 <input type="password" placeholder="Enter You Email" />
-                <button className="loginButton">Login</button>
+                <button className="loginButton">Register</button>
             </form>
-            <button className="loginRegisterButton">Register</button>
+            <button className="loginRegisterButton">Login</button>
         </div>
     )
 }
